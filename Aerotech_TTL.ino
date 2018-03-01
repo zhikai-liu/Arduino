dc9b141d22f2561cb629bc6ff1d49f3213d0c097
zhikai <zhikai@wustl.edu>
2018-03-01 10:28:51 -0600

    add TTL triggering code for aerotech-plcamp wiring

---
+++ Aerotech_TTL.ino
@@ -0,0 +1,27 @@
+int AeroIn=53;
+int TTLout=50;
+int Aero=0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(AeroIn,INPUT);
+  pinMode(TTLout,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Aero=digitalRead(AeroIn);
+  Serial.println(Aero);
+  if (Aero==HIGH)
+  {
+    digitalWrite(TTLout,HIGH);
+  }
+  if (Aero==LOW)
+  {
+    digitalWrite(TTLout,LOW);
+  }
+
+}
